e77deb5000d8a0b322ffe68eb1bbaccd81a8da20
Stephan Taylor <stephantaylor21@hotmail.com>
2022-02-16 15:53:31 -0700

    deleted js comments

--- 2-16-lab-gallery/js/main.js
+++ 2-16-lab-gallery/js/main.js
@@ -1,11 +1,3 @@
-// {
-//   albumId: 1,
-//   id: 8,
-//   title: "aut porro officiis laborum odit ea laudantium corporis",
-//   url: "https://via.placeholder.com/600/54176f",
-//   thumbnailUrl: "https://via.placeholder.com/150/54176f"
-// }
-
 const fetchImage = async function () {
   const response = await fetch("https://thatcopy.pw/catapi/rest/");
   const data = await response.json();
