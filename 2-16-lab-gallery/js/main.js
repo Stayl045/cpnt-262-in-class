e8af3ed774716c392f480ee32b02ecf0efdabbf9
Stephan Taylor <stephantaylor21@hotmail.com>
2022-02-16 14:01:34 -0700

    image commit

--- 2-16-lab-gallery/js/main.js
+++ 2-16-lab-gallery/js/main.js
@@ -7,15 +7,15 @@
 // }
 
 const fetchImage = async function () {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos/5");
+  const response = await fetch("https://random.dog/woof.json");
   const data = await response.json();
 
   console.log(data);
 
-  document.querySelector("h1").textContent = data.title;
-  document.querySelector("p").textContent = data.body;
+  document.querySelector("h1").fetchImage = data.title;
+  document.querySelector("p").fetchImage = data.body;
 };
 
 fetchImage();
 
-console.log("Hello world!");
+// console.log("Hello world!");
